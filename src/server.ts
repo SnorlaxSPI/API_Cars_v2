187b0459ce6d33bf269d712969b840d48745e3e7
aflinsjr@icloud.com <aflinsjr@icloud.com>
2022-02-10 10:03:27 -0300

    2 - Adicionando tipos

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { createCourse } from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', createCourse);
+
+app.listen(3333, () => {
+  console.log('🚀🚀 Server Started!')
+});
+
+export { app };
