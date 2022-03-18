11a01651a206c749b883cb565efe517429e3e54e
aflinsjr@icloud.com <aflinsjr@icloud.com>
2022-03-18 15:29:30 -0300

    5.4 Conceito de stream

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,10 +1,11 @@
 import { Request, Response } from 'express';
+
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
-class ImportCategoryController{
+class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) {}
   handle(request:Request, response:Response): Response {
-    const { file } = request;
+    const { file } : any = request;
 
     this.importCategoryUseCase.execute(file);
     
