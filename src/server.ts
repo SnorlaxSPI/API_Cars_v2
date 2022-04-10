f3575ecb5b0960f37debbebdd00c12ce47321212
aflinsjr@icloud.com <aflinsjr@icloud.com>
2022-04-10 10:38:48 -0300

    2.4 Inserindo ID com uuid

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { createCourse } from './routes';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', createCourse);
+app.use('/categories', categoriesRoutes);
 
 app.listen(3333, () => {
   console.log('🚀🚀 Server Started!')
